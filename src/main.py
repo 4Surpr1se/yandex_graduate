52b85660290e6a8fc32e127ca0ca3ce8c8301792
Alina Belko <alinabelko@gmail.com>
2024-08-06 19:15:16 +0200

    Добалено упраление lifespan для fastapi

--- src/main.py
+++ src/main.py
@@ -2,34 +2,32 @@ from elasticsearch import AsyncElasticsearch
 from fastapi import FastAPI
 from fastapi.responses import ORJSONResponse
 from redis.asyncio import Redis
+from contextlib import asynccontextmanager
 
 from api.v1 import films, genres, persons
 from core.config import config
 from db import elastic, redis
 
-app = FastAPI(
-    title=config.config.project_name,
-    docs_url='/api/openapi',
-    openapi_url='/api/openapi.json',
-    default_response_class=ORJSONResponse,
-)
-
-
-@app.on_event('startup')
-async def startup():
+@asynccontextmanager
+async def lifespan():
     redis.redis = Redis(host=config.redis_host, port=config.redis_port)
     elastic.es = AsyncElasticsearch([{
         'host': config.elastic_host,
         'port': config.elastic_port,
         'scheme': 'http'
     }])
-
-
-@app.on_event('shutdown')
-async def shutdown():
+    yield
     await redis.redis.close()
     await elastic.es.close()
-
+    
+app = FastAPI(
+    title=config.config.project_name,
+    docs_url='/api/openapi',
+    openapi_url='/api/openapi.json',
+    default_response_class=ORJSONResponse,
+    lifespan=lifespan
+)
+    
 
 app.include_router(films.router, prefix='/api/v1/films', tags=['films'])
 app.include_router(films.router, prefix='/api/v1/films', tags=['films'])
