b60ca69065fdeecdff691182eaa8792329751de1
Alina Belko <alinabelko@gmail.com>
2024-08-12 20:28:40 +0200

    #28 Исправлен формат настроек pydantic во всех проектах

--- postgres_to_es/settings.py
+++ postgres_to_es/settings.py
@@ -1,16 +1,14 @@
-from pydantic import Field
 from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
-    batch_size: int = Field(default=100, env='BATCH_SIZE')
-    elasticsearch_host: str = Field(
-        default='elasticsearch', env='ELASTIC_HOST')
-    elasticsearch_port: int = Field(default=9200, env='ELASTIC_PORT')
-    db_host: str = Field(env='DB_HOST')
-    db_name: str = Field(env='DB_NAME')
-    db_user: str = Field(env='DB_USER')
-    db_password: str = Field(env='DB_PASSWORD')
+    batch_size: int = 100
+    elastic_host: str
+    elastic_port: int = 9200
+    db_host: str
+    db_name: str
+    db_user: str
+    db_password: str
 
     class Config:
         env_file = "../.env"
--- tests/functional/settings.py
+++ tests/functional/settings.py
@@ -1,4 +1,3 @@
-from pydantic import Field
 from pydantic_settings import BaseSettings
 import os
 import sys 
@@ -6,9 +5,11 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')
 from testdata.es_mapping import es_index_mapping
 
 
-class TestSettings(BaseSettings):
-    es_host: str = Field('http://127.0.0.1:9200', env='ELASTIC_HOST')
-    redis_host: str = Field('http://127.0.0.1:6379', env='REDIS_HOST')
+class TestSettings(BaseSettings):   
+    redis_host: str = '127.0.0.1'
+    redis_port: str = '6379'
+    elastic_host: str = '127.0.0.1'
+    elastic_port: str = '9200'
     es_index: str = 'movies_test'
     es_index_mapping: dict = es_index_mapping
 
