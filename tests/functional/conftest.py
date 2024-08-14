609421b8cb6fd419dbf5f17e7391252f8c9fadcf
Alina Belko <alinabelko@gmail.com>
2024-08-14 18:10:57 +0200

    #28 Добавлена фикстура redis

---
+++ tests/functional/conftest.py
@@ -0,0 +1,9 @@
+import pytest
+import redis
+from settings import test_settings
+
+@pytest.fixture(scope='function')
+def redis_client():
+    client = redis.Redis(host=test_settings.redis_host, port=test_settings.redis_port, decode_responses=True)
+    yield client
+    client.flushdb() 
