410a7806fccca1465f6206391811c96b091f9fab
gasmed <gasfdp50@gmail.com>
2024-08-16 08:00:39 +0300

    Добавлено наследование шаблонов

--- src/services/film.py
+++ src/services/film.py
@@ -1,47 +1,22 @@
 from functools import lru_cache
-from typing import Optional
 
+from pydantic import BaseModel
 from elasticsearch import AsyncElasticsearch
 from fastapi import Depends
 from redis.asyncio import Redis
 
-from db.elastic import ElasticInter, get_elastic
+from db.elastic import get_elastic
 from db.redis import get_redis
 from models.film import Film
+from services.base_service import BaseSingleItemService
 
-FILM_CACHE_EXPIRE_IN_SECONDS = 60 * 5
 
-
-class FilmService(ElasticInter):
+class FilmService(BaseSingleItemService):
     def __init__(self, redis: Redis, elastic: AsyncElasticsearch):
-        self.redis = redis
-        self.elastic = elastic
+        super().__init__(redis, elastic)
         self.index = 'movies'
-
-    async def get_by_id(self, film_id: str) -> Optional[Film]:
-        film = await self._film_from_cache(film_id)
-        if not film:
-            film = await self._get_film_from_elastic(film_id)
-            if not film:
-                return None
-            await self._put_film_to_cache(film)
-        return film
-
-    async def _get_film_from_elastic(self, film_id: str) -> Optional[Film]:
-        film = await self._get_by_id_from_elastic(film_id)
-        return Film(**film) if film else None
-
-    async def _film_from_cache(self, film_id: str) -> Optional[Film]:
-        data = await self.redis.get(film_id)
-        if not data:
-            return None
-
-        film = Film.parse_raw(data)
-        return film
-
-    async def _put_film_to_cache(self, film: Film):
-        film_id_str = f'film:{str(film.uuid)}'
-        await self.redis.set(film_id_str, film.json(), FILM_CACHE_EXPIRE_IN_SECONDS)
+        self.model: BaseModel = Film
+        self.service_name = 'film'
 
 
 @lru_cache()
--- src/services/genre.py
+++ src/services/genre.py
@@ -1,84 +1,31 @@
-import hashlib
-import json
-import logging
 from functools import lru_cache
-from typing import List, Optional
+from pydantic import BaseModel
 
-from elasticsearch import AsyncElasticsearch, NotFoundError
+from elasticsearch import AsyncElasticsearch
 from fastapi import Depends
 from redis.asyncio import Redis
+from starlette.datastructures import QueryParams
 
-from db.elastic import ElasticInter, get_elastic
+from db.elastic import get_elastic
 from db.redis import get_redis
 from models.genre import Genre
+from services.base_service import BaseSingleItemService, BasePluralItemsService
 
-GENRES_CACHE_EXPIRE_IN_SECONDS = 60 * 5
 
-
-class GenreService(ElasticInter):
+class GenreService(BasePluralItemsService):
     def __init__(self, redis: Redis, elastic: AsyncElasticsearch):
-        self.redis = redis
-        self.elastic = elastic
+        super().__init__(redis, elastic)
         self.index = 'genres'
+        self.model: BaseModel = Genre
+        self.service_name = 'genres'
 
-    async def get_all_genres(self) -> List[Genre]:
-        cache_key = self._generate_cache_key('get_all_genres')
-        genres = await self._get_genres_from_cache(cache_key)
-        if not genres:
-            genres = await self._get_all_genres_from_elastic()
-            if not genres:
-                return []
-            await self._put_genres_to_cache(cache_key, genres)
-        return genres
-
-    async def _get_all_genres_from_elastic(self) -> List[Genre]:
-        body = {"query": {"match_all": {}}}
-        hits = await self._get_hits_from_elastic(body)
-        genres = [Genre(uuid=hit['_id'],
-                        name=hit['_source']['name'],
-                        description=hit['_source']['description'])
-                  for hit in hits]
-        return genres or None
-
-    async def get_genre_by_id(self, genre_id: str) -> Optional[Genre]:
-        cache_key = self._generate_cache_key(
-            'get_genre_by_id', genre_id=genre_id)
-        genre = await self._get_genre_from_cache(cache_key)
-        if not genre:
-            genre = await self._get_genre_by_id_from_elastic(genre_id)
-            if not genre:
-                return None
-            await self._put_genre_to_cache(cache_key, genre)
-        return genre
-
-    async def _get_genre_by_id_from_elastic(self, genre_id: str) -> Optional[Genre]:
-        genre = await self._get_by_id_from_elastic(genre_id)
-        return Genre(uuid=genre['id'],
-                     name=genre['name'],
-                     description=genre['description']) if genre else None
-
-    def _generate_cache_key(self, method: str, **kwargs) -> str:
-        query_str = json.dumps(kwargs, sort_keys=True)
-        key = f'{method}:{query_str}'
-        return hashlib.sha256(key.encode()).hexdigest()
-
-    async def _get_genres_from_cache(self, cache_key: str) -> Optional[List[Genre]]:
-        genres = await self.redis.get(cache_key)
-        if not genres:
-            return None
-        return [Genre.parse_raw(genre) for genre in json.loads(genres)]
-
-    async def _put_genres_to_cache(self, cache_key: str, genres: List[Genre]):
-        await self.redis.set(cache_key, json.dumps([genre.json() for genre in genres]), GENRES_CACHE_EXPIRE_IN_SECONDS)
-
-    async def _get_genre_from_cache(self, cache_key: str) -> Optional[Genre]:
-        genre = await self.redis.get(cache_key)
-        if not genre:
-            return None
-        return Genre.parse_raw(genre)
-
-    async def _put_genre_to_cache(self, cache_key: str, genre: Genre):
-        await self.redis.set(cache_key, genre.json(), GENRES_CACHE_EXPIRE_IN_SECONDS)
+    def _generate_body(self, query_params: QueryParams = None) -> dict:
+        size = int(query_params.get("size", 10000))
+        body = {
+            "query": {"match_all": {}},
+            "size": size
+        }
+        return body
 
 
 @lru_cache()
