37b752a3b552685b0c738b1ab65b68e9d60799c9
gasmed <gasfdp50@gmail.com>
2024-08-16 07:54:51 +0300

    Добавлена обработка поля id

--- src/models/genre.py
+++ src/models/genre.py
@@ -1,11 +1,17 @@
 from typing import Optional
 
-from pydantic import UUID4, BaseModel, Field
+from pydantic import BaseModel, UUID4, model_validator
 
 
 class GenreBase(BaseModel):
     uuid: UUID4
     name: str
+
+    @model_validator(mode='before')
+    def rename_id_to_uuid(cls, values):
+        if 'id' in values:
+            values['uuid'] = values.pop('id')
+        return values
 
 
 class Genre(GenreBase):
