e4ea0a518cbc96c7f10fa4319f0a42347eb3dfbd
Alina Belko <alinabelko@gmail.com>
2024-08-30 20:51:56 +0200

    #50 Добавлены токены в куки

--- auth_service/src/api/auth.py
+++ auth_service/src/api/auth.py
@@ -1,14 +1,14 @@
-# src/api/auth.py
-from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi import APIRouter, Depends, HTTPException, status, Response
 from sqlalchemy.ext.asyncio import AsyncSession
-from src.schemas.auth import UserAuth, Token
+from src.schemas.auth import UserAuth
 from src.services.auth import authenticate_user, create_tokens
 from src.db.postgres import get_session
+from src.core.config import settings
 
 router = APIRouter()
 
-@router.post('/login', response_model=Token, status_code=status.HTTP_200_OK)
-async def login(user_auth: UserAuth, db: AsyncSession = Depends(get_session)) -> Token:
+@router.post('/login', status_code=status.HTTP_200_OK)
+async def login(user_auth: UserAuth, response: Response, db: AsyncSession = Depends(get_session)):
     user = await authenticate_user(user_auth.login, user_auth.password, db)
     if not user:
         raise HTTPException(
@@ -16,4 +16,11 @@ async def login(user_auth: UserAuth, db: AsyncSession = Depends(get_session)) ->
             detail="Invalid username or password",
             headers={"WWW-Authenticate": "Bearer"},
         )
-    return await create_tokens(user)
+    
+    tokens = await create_tokens(user)
+    
+    response.set_cookie(key="access_token", value=tokens.access_token, httponly=True, max_age=60*settings.access_token_expire_minutes)
+    response.set_cookie(key="refresh_token", value=tokens.refresh_token, httponly=True, max_age=60*60*24*settings.refresh_token_expire_days)
+    
+    response.status_code = status.HTTP_200_OK
+    return response
