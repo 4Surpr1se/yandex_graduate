a0266ae7a185073cd21ed9a571702f579341d78c
gasmed <gasfdp50@gmail.com>
2024-09-02 13:09:38 +0300

    добавлены url'ы для доступа к методам обновления пароля и логина

--- auth_service/src/api/user.py
+++ auth_service/src/api/user.py
@@ -1,8 +1,9 @@
-from fastapi import APIRouter, Depends, status
+from fastapi import APIRouter, Depends, status, HTTPException, Request
 from sqlalchemy.ext.asyncio import AsyncSession
-from src.services.user import create_user_service  # импортируем сервис для создания пользователя
-from src.schemas.user import UserCreate, UserInDB
-from src.db.postgres import get_session  # Импортируем функцию получения сессии
+
+from src.services.user import create_user_service, update_login_service, update_password_service
+from src.schemas.user import UserCreate, UserInDB, UpdateResponse
+from src.db.postgres import get_session
 
 router = APIRouter()
 
@@ -10,3 +11,27 @@ router = APIRouter()
 @router.post('/signup', response_model=UserInDB, status_code=status.HTTP_201_CREATED)
 async def create_user(user_create: UserCreate, db: AsyncSession = Depends(get_session)) -> UserInDB:
     return await create_user_service(user_create, db)
+
+
+@router.post('/update-login', response_model=UpdateResponse, status_code=status.HTTP_200_OK)
+async def update_login(new_user_login: str,
+                       request: Request,
+                       db: AsyncSession = Depends(get_session)
+                       ):
+    access_token = request.cookies.get("access_token")
+    response = await update_login_service(db, new_user_login, access_token)
+    if not response:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
+    return response
+
+
+@router.post('/update-password', response_model=UpdateResponse, status_code=status.HTTP_200_OK)
+async def update_password(new_user_password: str,
+                          request: Request,
+                          db: AsyncSession = Depends(get_session)
+                          ):
+    access_token = request.cookies.get('access_token')
+    res = await update_password_service(db, new_user_password, access_token)
+    if not res:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
+    return res
