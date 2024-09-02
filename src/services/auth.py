bd76d145c5fa93ef28bd9df909e14a91be9b8e9b
Alina Belko <alinabelko@gmail.com>
2024-09-02 19:15:32 +0200

    #50 Добавлена отправка запроса на refresh если access_token истек

--- src/services/auth.py
+++ src/services/auth.py
@@ -8,15 +8,28 @@ security = HTTPBearer()
 
 async def verify_jwt(request: Request):
     token: Optional[str] = request.cookies.get('access_token')
+    refresh_token: Optional[str] = request.cookies.get('refresh_token')
     
     if not token:
         raise HTTPException(status_code=401, detail="No access token provided")
     
     headers = {"Cookie": request.headers.get("cookie")}
-
     auth_service_url = f"{settings.auth_service_url}/api/auth/verify_token"
     response = requests.post(auth_service_url, json={"token": token}, headers=headers)
     
+    if response.status_code == 401 and refresh_token:
+        refresh_url = f"{settings.auth_service_url}/api/auth/refresh"
+        refresh_response = requests.post(refresh_url, json={"refresh_token": refresh_token}, headers=headers)
+
+        if refresh_response.status_code == 200:
+            new_tokens = refresh_response.json()
+            token = new_tokens["access_token"]
+            
+            request.cookies["access_token"] = token
+            headers["Cookie"] = f'access_token={token}; refresh_token={refresh_token}'
+            
+            response = requests.post(auth_service_url, json={"token": token}, headers=headers)
+        
     if response.status_code != 200:
         try:
             detail = response.json().get("detail", "Unauthorized")
