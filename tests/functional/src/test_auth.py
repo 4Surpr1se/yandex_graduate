ba892cbd35a2d8cb12b6c3a0e718fa9df09c234f
Alina Belko <alinabelko@gmail.com>
2024-09-06 20:25:58 +0200

    #63 Добавлены тесты обращения к фильмам

--- tests/functional/src/test_auth.py
+++ tests/functional/src/test_auth.py
@@ -1,6 +1,7 @@
 import requests
 from settings import test_settings
 
+FILMS_URL = f"http://{test_settings.service_host}:{test_settings.service_port}/api/v1/films"
 
 def user_login(username, password, cookies=None):
     login_data = {
@@ -33,44 +34,61 @@ def test_user_registration_and_update():
         # Шаг 2: Логин
         cookies = user_login(username, initial_password)
 
-        # Шаг 3: Смена логина
-        new_username = test_settings.test_user_login+'1'
+        # Шаг 3: Проверка доступа к страницам фильмов (зарегистрированный пользователь)
+        # Доступ к 1 странице
+        response = requests.get(FILMS_URL, params={"page_size": "4", "page_number": 1}, cookies=cookies)
+        assert response.status_code == 200
+        data = response.json()
+        assert len(data) > 0
+
+        # Доступ ко 2 странице
+        response = requests.get(FILMS_URL, params={"page_size": "4", "page_number": 2}, cookies=cookies)
+        assert response.status_code == 200
+        data = response.json()
+        assert len(data) > 0
+
+        # Шаг 4: Смена логина
+        new_username = test_settings.test_user_login + '1'
         response = requests.post(f"{test_settings.auth_service_url}/api/users/update-login", params={"new_user_login": new_username}, cookies=cookies)
         assert response.status_code == 200
         
         cookies = user_login(new_username, initial_password)
 
-        # Шаг 4: Смена пароля
+        # Шаг 5: Смена пароля
         new_password = test_settings.test_user_password[::-1]
         response = requests.post(f"{test_settings.auth_service_url}/api/users/update-password", params={"new_user_password": new_password}, cookies=cookies)
         assert response.status_code == 200
 
-        # Шаг 5: Логин с новым паролем
+        # Шаг 6: Логин с новым паролем
         cookies = user_login(new_username, new_password)
 
-        # Шаг 6: Смена логина обратно
+        # Шаг 7: Смена логина обратно
         response = requests.post(f"{test_settings.auth_service_url}/api/users/update-login", params={"new_user_login": username}, cookies=cookies)
         assert response.status_code == 200
         
         cookies = user_login(username, new_password)
 
-        # Шаг 7: Смена пароля обратно
+        # Шаг 8: Смена пароля обратно
         response = requests.post(f"{test_settings.auth_service_url}/api/users/update-password", params={"new_user_password": initial_password}, cookies=cookies)
         assert response.status_code == 200
         
         cookies = user_login(username, initial_password)
 
-        # Шаг 8: Удаление access_token и обновление через refresh
-        #del cookies['access_token']
-        #response = requests.post(f"{test_settings.auth_service_url}/api/auth/refresh", cookies=cookies)
-        #assert response.status_code == 200
-        #new_cookies = response.cookies
-        #assert 'access_token' in new_cookies and new_cookies['access_token'] != cookies.get('access_token')
-
         # Шаг 9: Логаут
         response = requests.post(f"{test_settings.auth_service_url}/api/auth/logout", cookies=cookies)
         assert response.status_code == 200
         assert 'access_token' not in response.cookies and 'refresh_token' not in response.cookies
+
+        # Шаг 10: Проверка доступа к страницам фильмов (незарегистрированный пользователь)
+        # Доступ к 1 странице
+        response = requests.get(FILMS_URL, params={"page_size": "4", "page_number": 1})
+        assert response.status_code == 200
+        data = response.json()
+        assert len(data) > 0
+
+        # Попытка получить доступ ко 2 странице
+        response = requests.get(FILMS_URL, params={"page_size": "4", "page_number": 2})
+        assert response.status_code == 403
 
     finally:
         # Удаление пользователя
