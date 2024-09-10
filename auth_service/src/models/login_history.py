239a596abc971fd05b1a86d87d6ab715d2cb2aed
Alina Belko <alinabelko@gmail.com>
2024-09-10 12:53:16 +0200

    #80 Добавлено каскадное удаление user login history

--- auth_service/src/models/login_history.py
+++ auth_service/src/models/login_history.py
@@ -7,16 +7,17 @@ from sqlalchemy.dialects.postgresql import UUID
 
 from src.db.postgres import Base
 
+
 class UserLogin(Base):
     __tablename__ = 'user_login'
 
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, unique=True, nullable=False)
-    user_id = Column(UUID(as_uuid=True), ForeignKey('users.id'), nullable=False)
+    user_id = Column(UUID(as_uuid=True), ForeignKey('users.id', ondelete='CASCADE'), nullable=False)
     logged_at = Column(DateTime, default=datetime.utcnow)
-
 
     def __init__(self, user_id: UUID):
         self.user_id = user_id
 
     def __repr__(self):
         return f'Logged in at <{self.logged_at}>'
+
