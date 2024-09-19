edf4150efafb9841ea50b84db91700a409f0286a
gasmed <gasfdp50@gmail.com>
2024-09-19 04:26:10 +0300

    Добавлена админ панель

---
+++ admin_panel/app/config/settings/security.py
@@ -0,0 +1,26 @@
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.'
+                'UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.'
+                'MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.'
+                'CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.'
+                'NumericPasswordValidator',
+    },
+
+]
+
+AUTH_USER_MODEL = "movies.User"
+
+AUTHENTICATION_BACKENDS = [
+    'movies.auth_backend.CustomBackend',
+    # 'django.contrib.auth.backends.ModelBackend',
+]
