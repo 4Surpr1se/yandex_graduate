5a9e952d41c72898a746be5e3cd1d67d69e985d0
Alina Belko <alinabelko@gmail.com>
2024-11-08 22:01:55 +0100

    Оптимизирован шедулер

--- notification_generator/notification_generator.py
+++ notification_generator/notification_generator.py
@@ -1,46 +1,27 @@
-from handlers import RegistrationHandler, LikeHandler, DislikeHandler, NewEpisodeHandler
-from config import settings
-import pika
-import schedule
-import json
-import time
+import asyncio
 
-class NotificationGenerator:
-    def __init__(self):
-        self.connection = pika.BlockingConnection(pika.ConnectionParameters(host=settings.rabbitmq_host))
-        self.channel = self.connection.channel()
-        
-        self.queues = {
-            'users.register': RegistrationHandler(api_url=settings.notification_api_url, email_sender=settings.email_sender),
-            'user.liked': LikeHandler(),
-            'series.new_episode': NewEpisodeHandler(),
-        }
-
-        for queue_name in self.queues:
-            self.channel.queue_declare(queue=queue_name, durable=True)
-
-    def consume_messages(self, queue_name):
-        handler = self.queues[queue_name]
-
-        def callback(ch, method, properties, body):
-            message = json.loads(body)
-            handler.handle(message)
-
-        self.channel.basic_consume(queue=queue_name, on_message_callback=callback, auto_ack=True)
-
-    def schedule_jobs(self):
-        schedule.every(5).seconds.do(lambda: self.consume_messages('users.register'))
-        schedule.every(10).seconds.do(lambda: self.consume_messages('user.liked'))
-        schedule.every(15).seconds.do(lambda: self.consume_messages('series.new_episode'))
+class NotificationScheduler:
+    async def schedule_task(self, interval, queue_name):
+        while True:
+            self.consume_messages(queue_name)
+            await asyncio.sleep(interval)
 
     def start(self):
-        self.schedule_jobs()
-        print("Notification generator started with scheduler")
-        while True:
-            schedule.run_pending()
-            time.sleep(1)
+        print("Notification generator started with asyncio scheduler")
 
+        asyncio.run(self.run_scheduler())
+
+    async def run_scheduler(self):
+        tasks = [
+            asyncio.create_task(self.schedule_task(30, 'users.register')),
+            asyncio.create_task(self.schedule_task(60, 'user.liked')),
+            asyncio.create_task(self.schedule_task(120, 'series.new_episode')),
+        ]
+        await asyncio.gather(*tasks)
+
+    def consume_messages(self, queue_name):
+        print(f"Consuming messages from {queue_name}")
 
 if __name__ == "__main__":
-    generator = NotificationGenerator()
-    generator.start()
+    scheduler = NotificationScheduler()
+    scheduler.start()
