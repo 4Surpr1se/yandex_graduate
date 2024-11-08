6492fb40a405ff87a0fafbc2c84245c5a9490537
gasmed <gasfdp50@gmail.com>
2024-11-08 22:25:05 +0300

    modified queue selection

--- notification_api/app.py
+++ notification_api/app.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from typing import List, Optional
 from enum import Enum
-import uuid
+from uuid import UUID
 from message_sender import send_message
 
 app = FastAPI()
@@ -13,10 +13,18 @@ class ChannelType(Enum):
     BROWSER = 2
 
 
+class NotificationType(Enum):
+    MASS_MAILING = 1
+    WEEKLY_REPORT = 2
+    REGISTRATION = 3
+
+
 class Notification(BaseModel):
     title: str
     body: str
     channel_type: ChannelType
+    message_type: NotificationType
+    content_id: UUID
     recipients: List[str]
     usernames: Optional[List[str]] = None
 
@@ -25,9 +33,10 @@ class Notification(BaseModel):
 async def send_notification(notification: Notification):
     """
     Универсальный маршрут для отправки уведомлений.
-    Использует channel_type и список usernames для персонализации сообщений.
+    Использует channel_type, message_type и список usernames для персонализации сообщений.
     """
-    headers = {"X-Request-Id": str(uuid.uuid4())}
+    headers = {"X-Request-Id": str(uuid.uuid4()),
+               "channel_type": str(notification.channel_type.value)}
 
     if notification.recipients != ["all-users"] and notification.usernames:
         if len(notification.recipients) != len(notification.usernames):
@@ -42,11 +51,8 @@ async def send_notification(notification: Notification):
             </body>
             </html>
         """
-        queue_name = "emails.send-mass-email" if notification.channel_type == ChannelType.EMAIL else \
-            "browser.send-mass-notification"
-        send_message(queue_name, message_body, headers=headers)
-        return {"status": f"Mass {'email' if notification.channel_type == ChannelType.EMAIL else 'browser'} "
-                          f"notification sent to all users"}
+        send_message("email.send-mass-notification", message_body, headers=headers)
+        return {"status": f"Mass notification sent to all users, channel type: {notification.channel_type.name}"}
 
     for index, recipient in enumerate(notification.recipients):
         username = notification.usernames[index] if notification.usernames else "User"
@@ -60,8 +66,11 @@ async def send_notification(notification: Notification):
             </html>
         """
         headers["Recipient"] = recipient
-        queue_name = "emails.send-individual" if notification.channel_type == ChannelType.EMAIL \
-            else "browser.send-individual"
-        send_message(queue_name, personalized_body, headers=headers)
+        if notification.notification_type == NotificationType.REGISTRATION:
+            send_message("email.send-welcome", personalized_body, headers=headers)
+        elif notification.notification_type == NotificationType.WEEKLY_REPORT:
+            send_message("email.send-weekly-films", personalized_body, headers=headers)
+        else:
+            send_message("email.send-individual", personalized_body, headers=headers)
 
-    return {"status": "Individual messages sent"}
+    return {"status": "Individual message s sent"}
--- notification_api/config.py
+++ notification_api/config.py
@@ -7,10 +7,14 @@ class Settings(BaseSettings):
     RABBITMQ_PORT: int = 5672
     RABBITMQ_USER: str = "user"
     RABBITMQ_PASSWORD: str = "password"
+    POSTGRES_USE: str = "user"
+    POSTGRES_PASSWORD: str = "qwe123"
+    POSTGRES_DB: str = "notification"
 
     class Config:
         case_sensitive = True
         env_file = ".env"
+        extra = "ignore"
 
 
 settings = Settings()
