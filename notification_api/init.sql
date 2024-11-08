28df74057f543770e3c397be927730d6e608711e
gasmed <gasfdp50@gmail.com>
2024-11-09 00:40:43 +0300

    deleted previous version

--- notification_api/init.sql
+++
@@ -1,8 +0,0 @@
--- init.sql
-
-CREATE TABLE notifications (
-    notification_id UUID PRIMARY KEY,
-    content_id UUID NOT NULL,
-    last_update TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    last_notification_send TIMESTAMP
-);
