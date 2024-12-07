c6a6fdd3c50c1dadda64fc5b34ba6292697c6a56
Александра Щетинина <aleksandra@MacBook-Pro-Aleksandra.local>
2024-12-07 19:26:33 +0300

    внесены правки

--- billing_api/services.py
+++ billing_api/services.py
@@ -1,79 +1,87 @@
-import requests
+import httpx
 from fastapi import Depends, HTTPException, Request
 from jose import JWTError, jwt
 from config import settings
+from http import HTTPStatus
 
 
 # Извлечение токена из куков и декодирование
-def get_user_id_from_token(request: Request) -> str:
+async def get_user_id_from_token(request: Request) -> str:
     access_token = request.cookies.get("access_token")
     if not access_token:
-        raise HTTPException(status_code=401, detail="No access token provided")
+        raise HTTPException(status_code=HTTPStatus.UNAUTHORIZED, detail="No access token provided")
 
     try:
         payload = jwt.decode(access_token, settings.jwt_secret_key, algorithms=[settings.jwt_algorithm])
         user_id: str = payload.get("user_id")
         if user_id is None:
-            raise HTTPException(status_code=403, detail="Could not validate credentials")
+            raise HTTPException(status_code=HTTPStatus.FORBIDDEN, detail="Could not validate credentials")
         return user_id
     except JWTError:
-        raise HTTPException(status_code=403, detail="Could not validate credentials")
+        raise HTTPException(status_code=HTTPStatus.FORBIDDEN, detail="Could not validate credentials")
 
 
-def get_user_mail_from_token(request: Request) -> str:
+async def get_user_mail_from_token(request: Request) -> str:
     access_token = request.cookies.get("access_token")
     if not access_token:
-        raise HTTPException(status_code=401, detail="No access token provided")
+        raise HTTPException(status_code=HTTPStatus.UNAUTHORIZED, detail="No access token provided")
+
     try:
         payload = jwt.decode(access_token, settings.jwt_secret_key, algorithms=[settings.jwt_algorithm])
         user_mail: str = payload.get("mail")
+        if user_mail is None:
+            raise HTTPException(status_code=HTTPStatus.FORBIDDEN, detail="Could not validate credentials")
         return user_mail
     except JWTError:
-        raise HTTPException(status_code=403, detail="Could not validate credentials")
+        raise HTTPException(status_code=HTTPStatus.FORBIDDEN, detail="Could not validate credentials")
 
 
 # Получить стоимость фильма через price_service
-def get_film_price(country: str, film_id: str) -> dict:
-    response = requests.post(
-        f"{settings.price_service_url}/calculate_film_price",
-        json={"film_id": str(film_id), "country": country}
-    )
-    if response.status_code == 200:
-        return response.json()  # Возвращаем данные о стоимости фильма
-    else:
-        raise Exception("Error getting film price")
+async def get_film_price(country: str, film_id: str) -> dict:
+    async with httpx.AsyncClient() as client:
+        response = await client.post(
+            f"{settings.price_service_url}/calculate_film_price",
+            json={"film_id": str(film_id), "country": country}
+        )
+        if response.status_code == HTTPStatus.OK:
+            return response.json()  # Возвращаем данные о стоимости фильма
+        else:
+            raise HTTPException(status_code=HTTPStatus.INTERNAL_SERVER_ERROR, detail="Error getting film price")
 
 
 # Получить стоимость подписки через price_service
-def get_subscription_price(country: str, subscription_type: str) -> dict:
-    response = requests.post(
-        f"{settings.price_service_url}/calculate_subscription_price",
-        json={"subscription_type": subscription_type, "country": country}
-    )
-    if response.status_code == 200:
-        return response.json()  # Возвращаем данные о стоимости подписки
-    else:
-        raise Exception("Error getting subscription price")
+async def get_subscription_price(country: str, subscription_type: str) -> dict:
+    async with httpx.AsyncClient() as client:
+        response = await client.post(
+            f"{settings.price_service_url}/calculate_subscription_price",
+            json={"subscription_type": subscription_type, "country": country}
+        )
+        if response.status_code == HTTPStatus.OK:
+            return response.json()  # Возвращаем данные о стоимости подписки
+        else:
+            raise HTTPException(status_code=HTTPStatus.INTERNAL_SERVER_ERROR, detail="Error getting subscription price")
 
 
 # Инициализация платежа через billing_service
-def initiate_payment(payment_data: dict) -> dict:
-    response = requests.post(
-        f"{settings.billing_service_url}/api/v1/payment/create-payment",
-        json=payment_data
-    )
-    if response.status_code == 200:
-        return response.json()  # Возвращаем данные о платеже
-    else:
-        raise Exception("Error initiating payment")
+async def initiate_payment(payment_data: dict) -> dict:
+    async with httpx.AsyncClient() as client:
+        response = await client.post(
+            f"{settings.billing_service_url}/api/v1/payment/create-payment",
+            json=payment_data
+        )
+        if response.status_code == HTTPStatus.OK:
+            return response.json()  # Возвращаем данные о платеже
+        else:
+            raise HTTPException(status_code=HTTPStatus.INTERNAL_SERVER_ERROR, detail="Error initiating payment")
 
 
 # Отмена подписки через billing_service
-def cancel_subscription(subscription_id: str) -> dict:
-    response = requests.post(
-        f"{settings.billing_service_url}/api/v1/payment/cancel/{subscription_id}"
-    )
-    if response.status_code == 200:
-        return response.json()  # Возвращаем результат отмены подписки
-    else:
-        raise Exception("Error canceling subscription")
+async def cancel_subscription(subscription_id: str) -> dict:
+    async with httpx.AsyncClient() as client:
+        response = await client.post(
+            f"{settings.billing_service_url}/api/v1/payment/cancel/{subscription_id}"
+        )
+        if response.status_code == HTTPStatus.OK:
+            return response.json()  # Возвращаем результат отмены подписки
+        else:
+            raise HTTPException(status_code=HTTPStatus.INTERNAL_SERVER_ERROR, detail="Error canceling subscription")
